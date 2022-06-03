384b2bd0ad690ca45ed79d4b9aa16371cf9c73d1
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-06-03 20:24:47 +0200

    feat: build ast

--- injector/injector.py
+++ injector/injector.py
@@ -1,8 +1,11 @@
+import ast
+
 class Injector:
     
-    def __init__(self, source):
-        self.source = source
-        self.raw_source = ''.join(source)
+    def __init__(self, fileName):
+        self.source = open(fileName, 'r').readlines()
+        self.raw_source = ''.join(self.source)
+        root_node = ast.parse(self.raw_source, fileName)
 
     def inject(self, fault_type):
         print('Hi')
--- ml_fault_injector.py
+++ ml_fault_injector.py
@@ -7,9 +7,11 @@ from injector.injector import Injector
 if __name__ == '__main__':
     break_repair = sys.argv[1] # break or repair
     fault_type = sys.argv[2]
-    file = sys.argv[3]
+    fileName = sys.argv[3]
     
-    source = open(file, 'r').readlines()
-
-    injector = Injector(source)
-    injector.inject(fault_type)
+    try:
+        if break_repair == 'break':
+            injector = Injector(fileName)
+            injector.inject(fault_type)
+    except Exception as e:
+        print('ERROR: ' + str(e))
