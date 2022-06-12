fb1c92df94e899fc2fc8160a6975a224ae1fa0ad
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-06-12 13:19:51 +0200

    feat: use same function for both workers

---
+++ shared/inject_functions.py
@@ -0,0 +1,46 @@
+from shared.utils import change_line_source
+from shared.ast_parser import Visitor
+
+def causeOutOfMemoryException(source, searchString, visitor: Visitor):
+    funcLines = [idx for idx, s in enumerate(source) if searchString in s]
+    if len(funcLines) == 0:
+        return None
+    
+    newSource = None
+    batchSizeName = 'batch_size'
+    index = funcLines[0]
+    func = visitor.lineno_function_call[index + 1][0]
+
+    fun_params = visitor.func_key_raw_params[func]
+    
+    batchSizeParam = None
+    for varName, rawParam in fun_params:
+        if varName is not None and batchSizeName in varName:
+            batchSizeParam = rawParam
+            break
+
+    varValue = batchSizeParam.name
+    if varValue.isdigit():
+        newSource = change_line_source(
+            source,
+            batchSizeParam.start_lineno - 1,
+            batchSizeName + '=' + varValue,
+            batchSizeName + '=' + str(int(varValue) * int(varValue))
+        )
+    else:
+        varLines = [idx for idx, s in enumerate(source) if varValue in s]
+        varLines.reverse()
+        
+        for line in varLines:
+            var = visitor.lineno_varname[line + 1][0]
+            if var == varValue:
+                varvalue = visitor.lineno_varvalue[line + 1][0]
+                newSource = change_line_source(
+                    source,
+                    line,
+                    str(varvalue.value),
+                    str(int(varvalue.value) * int(varvalue.value))
+                )
+                break
+
+    return newSource
--- worker_pytorch.py
+++ worker_pytorch.py
@@ -1,5 +1,6 @@
 from shared.ast_parser import Visitor
 from shared.utils import change_line_source
+from shared.inject_functions import causeOutOfMemoryException
 
 class WorkerPyTorch:
     
@@ -8,48 +9,7 @@ class WorkerPyTorch:
         self.visitor = visitor
 
     def causeOutOfMemoryException(self):
-        funcLines = [idx for idx, s in enumerate(self.source) if 'DataLoader(' in s]
-        if len(funcLines) == 0:
-            return None
-        
-        newSource = None
-        batchSizeName = 'batch_size'
-        index = funcLines[0]
-        func = self.visitor.lineno_function_call[index + 1][0]
-
-        fun_params = self.visitor.func_key_raw_params[func]
-        
-        batchSizeParam = None
-        for varName, rawParam in fun_params:
-            if varName is not None and batchSizeName in varName:
-                batchSizeParam = rawParam
-                break
-
-        varValue = batchSizeParam.name
-        if varValue.isdigit():
-            newSource = change_line_source(
-                self.source,
-                batchSizeParam.start_lineno - 1,
-                batchSizeName + '=' + varValue,
-                batchSizeName + '=' + str(int(varValue) * int(varValue))
-            )
-        else:
-            varLines = [idx for idx, s in enumerate(self.source) if varValue in s]
-            varLines.reverse()
-            
-            for line in varLines:
-                var = self.visitor.lineno_varname[line + 1][0]
-                if var == varValue:
-                    varvalue = self.visitor.lineno_varvalue[line + 1][0]
-                    newSource = change_line_source(
-                        self.source,
-                        line,
-                        str(varvalue.value),
-                        str(int(varvalue.value) * int(varvalue.value))
-                    )
-                    break
-
-        return newSource
+        return causeOutOfMemoryException(self.source, 'DataLoader(', self.visitor)
 
     def inject(self, faultType):
         if faultType == 'memory':
