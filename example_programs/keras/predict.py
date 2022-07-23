e75aa505cdb2764dd4449e6b94a55b9dd05ead6d
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-07-23 18:44:33 +0200

    chore: add normalisation step in test

--- example_programs/keras/predict.py
+++ example_programs/keras/predict.py
@@ -2,6 +2,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import LayerNormalization
 from keras import backend as K
 import numpy as np
 import cv2
@@ -23,6 +24,7 @@ def create_model():
   model.add(Conv2D(32, (3, 3)))
   model.add(Activation('relu'))
   model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(LayerNormalization(axis=3 , center=True , scale=True))
 
   model.add(Conv2D(64, (3, 3)))
   model.add(Activation('relu'))
