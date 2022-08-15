56f7194a019db28fc05de6fd16ee2283af00022f
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-08-15 16:19:15 +0200

    feat: add memory fault in machine learning

--- worker_scikit.py
+++ worker_scikit.py
@@ -1,17 +1,41 @@
+from imp import source_from_cache
 from shared.utils import change_line_source
 import random
 import shared.inject_functions as injections
 from shared.ast_parser import Visitor
 
 class WorkerScitkit:
-    
     def __init__(self, source, visitor: Visitor):
         self.source = source
         self.visitor = visitor
 
+    def causeOutOfMemoryException(self):
+        pandaFunctions = [
+            '.json_normalize',
+            '.read_json',
+            '.read_csv'
+        ]
+
+        newSource = None
+        for function in pandaFunctions:
+            funcs = injections.getFuncs(self.visitor, function)
+            if len(funcs) == 0:
+                continue
+
+            func = funcs[0]
+            newSource = change_line_source(
+                self.source,
+                func.lineno - 1,
+                '\n',
+                '* 200000000000000000\n'
+            )
+            break
+
+        return newSource
+
     def inject(self, faultType):
         if faultType == 'memory':
-            return injections.causeOutOfMemoryException(self.source, 'DataLoader', self.visitor)
+            return self.causeOutOfMemoryException()
         elif faultType == 'delay':
             return injections.addDelay(self.source, 'cosine_similarity', self.visitor)
         else:
